1cc18fc5aa77803521729a2d5e11c19b1bed7b4b
George Kokonas <g.kokwnas@gmail.com>
2023-04-28 19:52:11 +0300

    add user registration funtionallity. jwt generation pending

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,52 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/UserModel");
+const SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
+//middleware for handling exceptions. read more : https://www.npmjs.com/package/express-async-handler
+const asyncHandler = require("express-async-handler");
+
 // descr : Register new user
 // route : users/register
 // req   : POST
-const registerUser = (req, res) => {
-  res.send({ message: "register user" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+
+  if (!email || !username || !password) {
+    res.status(400);
+    throw new Error("Please add missing field. All fields are required...");
+  }
+
+  //Check if user is already registered
+  const emailExists = await User.findOne({ email });
+  const usernameExists = await User.findOne({ username });
+
+  if (emailExists) {
+    res.status(400);
+    throw new Error("Email already registered");
+  } else if (usernameExists) {
+    res.status(400);
+    throw new Error("Username already registered");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(SALT_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+  const user = await User.create({
+    email,
+    username,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).send(user);
+  } else {
+    res.status(400);
+    throw new Error("Invalid provided data...");
+  }
+});
 
 // descr : Authenticate user
 // route : users/login
