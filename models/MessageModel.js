d53453bcd8103375258e874303edb6797b171c69
George Kokonas <g.kokwnas@gmail.com>
2023-05-15 19:34:48 +0300

    add timestamp to message model to display when message was sent

--- models/MessageModel.js
+++ models/MessageModel.js
@@ -2,17 +2,22 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema(
   {
-    roomId:{
-      type: String
+    roomId: {
+      type: String,
     },
-    senderId:{
-      type:String
+    senderId: {
+      type: String,
     },
-    text:{
-      type: String
-    }
+    text: {
+      type: String,
+    },
   },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: false,
+    },
+  }
 );
 
 module.exports = mongoose.model("Message", MessageSchema);
- 
